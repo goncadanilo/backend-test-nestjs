925d4de1035ac46e8e95e7438d667a121640ae78
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-05 11:00:45 -0300

    feat: create database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'aftersale-backend-test',
+      entities: [resolve(__dirname, 'modules', '**', 'entity', '*{.ts,.js}')],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
