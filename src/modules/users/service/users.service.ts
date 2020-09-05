08a0f6dfe7835cbf7b9b8578334dd7f7d7b7f41f
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-05 13:44:02 -0300

    feat: should not store user with duplicate email

--- src/modules/users/service/users.service.ts
+++ src/modules/users/service/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { StoreUserDto } from '../dtos/store-user.dto';
@@ -9,7 +9,14 @@ export class UsersService {
   constructor(@InjectRepository(Users) private repository: Repository<Users>) {}
 
   async store(data: StoreUserDto): Promise<number> {
+    const userAlreadyExists = await this.repository.findOne(data.email);
+
+    if (userAlreadyExists) {
+      throw new BadRequestException('User with same email already exists');
+    }
+
     const { id } = await this.repository.save(data);
+    this.repository.findOne();
 
     return id;
   }
