a267de4ec33a481888445942dcde7b03e762cca3
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-05 19:46:34 -0300

    test: should add products in favorites

---
+++ src/modules/products/service/products.service.spec.ts
@@ -0,0 +1,28 @@
+import { Repository } from 'typeorm';
+import { Products } from '../entity/products.entity';
+import { ProductsService } from './products.service';
+
+describe('ProductsService', () => {
+  let service: ProductsService;
+  let repository: Repository<Products>;
+
+  beforeEach(async () => {
+    repository = new Repository();
+    service = new ProductsService(repository);
+  });
+
+  describe('add', () => {
+    it('should add products in favorites', async () => {
+      const result = new Products();
+      jest.spyOn(repository, 'save').mockImplementation(async () => result);
+
+      expect(
+        await service.add({
+          id: 1,
+          title: 'any_title',
+          userId: 1,
+        }),
+      ).toHaveProperty('id');
+    });
+  });
+});
