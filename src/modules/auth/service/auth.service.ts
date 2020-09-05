e34d69f2498fcd7a48690b53e6417df3d5e5540f
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-05 17:53:19 -0300

    feat: shoult return an authentication token

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { compareSync } from 'bcryptjs';
 import { UsersService } from '../../users/service/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(userEmail: string, userPassword: string) {
     const user = await this.usersService.findByEmail(userEmail);
 
     if (user && compareSync(userPassword, user.password)) {
-      const { id, name, email } = user;
-      return { id, name, email };
+      const { id, email } = user;
+      return { id, email };
     }
 
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+    return {
+      token: this.jwtService.sign(payload),
+    };
   }
 }
