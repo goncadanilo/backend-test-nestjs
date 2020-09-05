08f3ac7a1d5ad1784f57b1b06eb2876bc75c3cb1
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-05 18:48:06 -0300

    feat: create products migration

---
+++ src/infra/typeorm/migrations/1599341780897-CreateProducts.ts
@@ -0,0 +1,59 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateProducts1599341780897 implements MigrationInterface {
+  private table = new Table({
+    name: 'products',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+      },
+      {
+        name: 'title',
+        type: 'varchar',
+        length: '255',
+        isNullable: false,
+      },
+      {
+        name: 'user_id',
+        type: 'int',
+        isNullable: false,
+      },
+      {
+        name: 'created_at',
+        type: 'datetime',
+        isNullable: false,
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'datetime',
+        isNullable: false,
+        default: 'now()',
+      },
+    ],
+  });
+
+  private foreignKey = new TableForeignKey({
+    columnNames: ['user_id'],
+    referencedColumnNames: ['id'],
+    onDelete: 'CASCADE',
+    referencedTableName: 'users',
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+    await queryRunner.createForeignKey(this.table, this.foreignKey);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(this.table, this.foreignKey);
+    await queryRunner.dropTable(this.table);
+  }
+}
