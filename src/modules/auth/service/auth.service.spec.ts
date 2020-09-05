f0bb84e7d2f245da09de797ea7f14ee99717dea8
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-05 17:53:31 -0300

    test: shoult return an authentication token

--- src/modules/auth/service/auth.service.spec.ts
+++ src/modules/auth/service/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { JwtService } from '@nestjs/jwt';
 import { hashSync } from 'bcryptjs';
 import { Repository } from 'typeorm';
 import { Users } from '../../users/entity/users.entity';
@@ -8,11 +9,13 @@ describe('AuthService', () => {
   let authService: AuthService;
   let userService: UsersService;
   let repository: Repository<Users>;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     repository = new Repository();
     userService = new UsersService(repository);
-    authService = new AuthService(userService);
+    jwtService = new JwtService({ secret: 'any_secret_key' });
+    authService = new AuthService(userService, jwtService);
   });
 
   describe('validateUser', () => {
@@ -29,7 +32,7 @@ describe('AuthService', () => {
 
       expect(
         await authService.validateUser('any@email.com', 'any_password'),
-      ).toStrictEqual({ id: 1, name: 'any_name', email: 'any@email.com' });
+      ).toStrictEqual({ id: 1, email: 'any@email.com' });
     });
 
     it('should return null when an incorrect email is provided', async () => {
@@ -53,6 +56,14 @@ describe('AuthService', () => {
       expect(
         await authService.validateUser('any@email.com', 'another_password'),
       ).toBe(null);
+    });
+  });
+
+  describe('login', () => {
+    it('shoult return an authentication token', async () => {
+      expect(
+        await authService.login({ email: 'any@email.com', userId: 'any_id' }),
+      ).toHaveProperty('token');
     });
   });
 });
