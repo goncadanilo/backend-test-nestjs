bdec549c70fbf993160e78c942b8fcb0c421f3c9
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-06 16:39:49 -0300

    refactor: change ormconfig

--- src/infra/typeorm/ormconfig.ts
+++ src/infra/typeorm/ormconfig.ts
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'aftersale-backend-test',
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   entities: ['src/modules/**/entity/*{.ts,.js}'],
   migrations: ['src/infra/typeorm/migrations/*{.ts,.js}'],
   cli: {
